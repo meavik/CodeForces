ca480be4e3fb7f47fb651d182f09432b41b7c5ba
ShaunAshraf <33549886+meavik@users.noreply.github.com>
2021-03-12 12:54:38 +0600

    Update A. Helpful Maths.cpp

--- A. Helpful Maths.cpp
+++ A. Helpful Maths.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main()
 {
     string s;
-    int flag;
+    int flag,i,j;
     cin>>s;
-    for(int i=0;i<s.size();i+=2){
+    for(i=0;i<s.size();i+=2){
         flag=0;
-        for(int j=0;j<s.size()-2;j+=2){
+        for(j=0;j<s.size()-1-i;j+=2){
             if(s[j]>s[j+2]){
                 swap(s[j],s[j+2]);
                 flag=1;
