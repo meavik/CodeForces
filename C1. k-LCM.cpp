35a0dc14ba00d18b50b57220ae06f19c49927948
ShaunAshraf <33549886+meavik@users.noreply.github.com>
2021-03-18 00:51:11 +0600

    Create C1. k-LCM.cpp
    It is the easy version of the problem. The only difference is that in this version k=3.
    
    You are given a positive integer n. Find k positive integers a1,a2,…,ak, such that:
    
    a1+a2+…+ak=n
    LCM(a1,a2,…,ak)≤n2
    Here LCM is the least common multiple of numbers a1,a2,…,ak.
    
    We can show that for given constraints the answer always exists.
    
    Input
    The first line contains a single integer t (1≤t≤104)  — the number of test cases.
    
    The only line of each test case contains two integers n, k (3≤n≤109, k=3).
    
    Output
    For each test case print k positive integers a1,a2,…,ak, for which all conditions are satisfied.

---
+++ C1. k-LCM.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+    int n,k;
+    cin>>n>>k;
+    if(n%2!=0){
+        cout<<n/2<<" "<<n/2<<" "<<1<<endl;
+    }
+    else if(n%4==0){
+        cout<<n/2<<" "<<n/4<<" "<<n/4<<endl;
+    }
+    else{
+        cout<<(n-1)/2<<" "<<(n-1)/2<<" "<<2<<endl;
+    }
+    }
+    return 0;
+}
